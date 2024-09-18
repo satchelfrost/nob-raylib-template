c55253266c11710ab7a0dff76ae8b6338102e6cd
Reese Gallagher <satchelfrost@gmail.com>
2024-09-17 23:30:42 -0400

    rotate, translate, scale is working

--- src/main.c
+++ src/main.c
@@ -11,7 +11,8 @@
 #define SCREEN_FACTOR 80
 #define SCREEN_WIDTH  (16 * SCREEN_FACTOR)
 #define SCREEN_HEIGHT ( 9 * SCREEN_FACTOR)
-#define CUBE_SPEED 5.0f
+#define CUBE_SPEED 3.0f
+#define NUM_TARGET_CUBES 10
 
 void update_camera_gamepad(Camera *camera)
 {
@@ -27,12 +28,19 @@ void update_camera_gamepad(Camera *camera)
     if (IsGamepadButtonDown(0, GAMEPAD_BUTTON_LEFT_TRIGGER_2))  CameraMoveUp(camera, -move_speed);
 }
 
+typedef enum {
+    MODE_FILLED,
+    MODE_WIREFRAME,
+} Mode;
+
 typedef struct {
     Vector3 pos;
     Vector3 forward;
     Vector3 up;
     Vector3 size;
     Color color;
+    Matrix m;
+    Mode mode;
 } Cube;
 
 typedef struct {
@@ -44,7 +52,7 @@ typedef struct {
 int main()
 {
     Camera camera = {
-        .position   = {0.0f, 3.0f, 5.0f},
+        .position   = {0.0f, 3.0f, 10.0f},
         .target     = {0.0f, 0.0f, 0.0f},
         .up         = {0.0f, 1.0f, 0.0f},
         .fovy       = 45,
@@ -59,6 +67,9 @@ int main()
         .color   = RED,
     };
 
+    Matrix target_cubes[NUM_TARGET_CUBES];
+    int target_cube_count = 0;
+
     Sphere sphere = {
         .pos = {0.0f, 0.0f, 0.0f},
         .color = MAGENTA,
@@ -70,34 +81,67 @@ int main()
     while(!WindowShouldClose()) {
         float dt = GetFrameTime();
 
-        /* update */
+        /* input */
         Vector3 forward = Vector3Normalize(cube.forward);
         Vector3 up = Vector3Normalize(cube.up);
         Vector3 right = Vector3CrossProduct(forward, up);
-        if (IsKeyDown(KEY_W)) cube.pos = Vector3Add(cube.pos, Vector3Scale(forward,  dt * CUBE_SPEED));
-        if (IsKeyDown(KEY_S)) cube.pos = Vector3Add(cube.pos, Vector3Scale(forward, -dt * CUBE_SPEED));
-        if (IsKeyDown(KEY_D)) cube.pos = Vector3Add(cube.pos, Vector3Scale(right,    dt * CUBE_SPEED));
-        if (IsKeyDown(KEY_A)) cube.pos = Vector3Add(cube.pos, Vector3Scale(right,   -dt * CUBE_SPEED));
-
-        // Matrix m = MatrixIdentity();
+        float speed = (IsKeyDown(KEY_LEFT_SHIFT)) ? dt * CUBE_SPEED * 0.25f : dt * CUBE_SPEED;
+        if (IsKeyDown(KEY_W)) cube.pos = Vector3Add(cube.pos, Vector3Scale(forward,  speed));
+        if (IsKeyDown(KEY_S)) cube.pos = Vector3Add(cube.pos, Vector3Scale(forward, -speed));
+        if (IsKeyDown(KEY_D)) cube.pos = Vector3Add(cube.pos, Vector3Scale(right,    speed));
+        if (IsKeyDown(KEY_A)) cube.pos = Vector3Add(cube.pos, Vector3Scale(right,   -speed));
         if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
             Vector2 delta = GetMouseDelta();
             Vector3 rot_y = Vector3RotateByAxisAngle(forward, up,    -delta.x * CAMERA_MOUSE_MOVE_SENSITIVITY);
             Vector3 rot_x = Vector3RotateByAxisAngle(forward, right, -delta.y * CAMERA_MOUSE_MOVE_SENSITIVITY);
             cube.forward = Vector3Normalize(Vector3Add(rot_y, rot_x));
         }
+        if (IsKeyPressed(KEY_SPACE)) {
+            if (target_cube_count + 1 <= NUM_TARGET_CUBES) target_cubes[target_cube_count++] = cube.m;
+            else TraceLog(LOG_INFO, "max cubes reached");
+        }
 
-        /* update sphere pos */
-        sphere.pos = Vector3Add(Vector3Scale(forward, 2.0f), cube.pos);
+        /* update  cube matrix */
+        Matrix r = MatrixInvert(MatrixLookAt((Vector3){0.0f, 0.0f, 0.0f}, cube.forward, up));
+        Matrix t = MatrixTranslate(cube.pos.x, cube.pos.y, cube.pos.z);
+        float wheel = GetMouseWheelMove();
+        if (wheel > 0.8)  {
+            cube.size.x += 0.1f;
+            cube.size.y += 0.1f;
+            cube.size.z += 0.1f;
+        }
+        if (wheel < -0.8) {
+            cube.size.x -= 0.1f;
+            cube.size.y -= 0.1f;
+            cube.size.z -= 0.1f;
+        }
+        Matrix s = MatrixScale(cube.size.x, cube.size.y, cube.size.z);
+        cube.m = MatrixMultiply(s, MatrixMultiply(r, t));
 
         /* drawing */
         BeginDrawing();
             ClearBackground(RAYWHITE);
             BeginMode3D(camera);
-                DrawGrid(100, 1);
-                DrawLine3D(cube.pos, sphere.pos, BLACK);
-                DrawSphere(sphere.pos, sphere.radius, sphere.color);
-                DrawCubeV(cube.pos, cube.size, cube.color);
+                DrawGrid(10, 10);
+                /* target cube */
+                for (int i = 0; i < target_cube_count; i++) {
+                    rlPushMatrix();
+                        rlMultMatrixf(MatrixToFloatV(target_cubes[i]).v);
+                        DrawCubeWiresV((Vector3){0.0f, 0.0f, 0.0f}, (Vector3){1.0f, 1.0f, 1.0f}, BLACK);
+                        DrawLine3D((Vector3){0.0f, 0.0f, -0.5f}, (Vector3){0.0f, 0.0f, -2.0f}, BLACK);
+                        rlTranslatef(0.0f, 0.0f, -2.0f);
+                        DrawSphere((Vector3){0.0f, 0.0f, 0.0f}, sphere.radius, sphere.color);
+                    rlPopMatrix();
+                }
+
+                /* user controlled cube */
+                rlPushMatrix();
+                    rlMultMatrixf(MatrixToFloatV(cube.m).v);
+                    DrawCubeV((Vector3){0.0f, 0.0f, 0.0f}, (Vector3){1.0f, 1.0f, 1.0f}, cube.color);
+                    DrawLine3D((Vector3){0.0f, 0.0f, -0.5f}, (Vector3){0.0f, 0.0f, -2.0f}, BLACK);
+                    rlTranslatef(0.0f, 0.0f, -2.0f);
+                    DrawSphere((Vector3){0.0f, 0.0f, 0.0f}, sphere.radius, sphere.color);
+                rlPopMatrix();
             EndMode3D();
         EndDrawing();
     }
